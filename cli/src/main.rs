185c19fc9539ddab2c4b7f3f4d8c302735109892
Epii <frank.g.taylor97@gmail.com>
2026-08-22 02:54:04 +0200

    cli: route current-main trust closure before release paths

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -6,6 +6,7 @@ mod composition {
     include!("first_suite.rs");
     include!("first_suite_finalize.rs");
     include!("suite_v2.rs");
+    include!("trust_closure.rs");
     include!("frontdoor.rs");
     include!("existing_world.rs");
 }
