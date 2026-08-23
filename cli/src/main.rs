fb00895f898d243b5d1837badc75c5a033b8a491
Epii <frank.g.taylor97@gmail.com>
2026-08-23 16:25:12 +0200

    feat: include current world front-door command

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -6,6 +6,7 @@ mod composition {
     include!("first_suite.rs");
     include!("first_suite_finalize.rs");
     include!("suite_v2.rs");
+    include!("current_world_command.rs");
     include!("frontdoor.rs");
     include!("existing_world.rs");
 }
