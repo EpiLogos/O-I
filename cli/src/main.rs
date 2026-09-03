8c2cc02e4f5dc0a88be74c0cf3026e523b1120cc
Epii <frank.g.taylor97@gmail.com>
2026-09-03 14:16:33 +0100

    [OI97-CLI] Include current-main native installer

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -9,6 +9,7 @@ mod composition {
     include!("current_world_command.rs");
     include!("trust_closure.rs");
     include!("trust_closure_guard.rs");
+    include!("current_main_install.rs");
     include!("existing_world.rs");
     include!("omarchy_host.rs");
     include!("product_command_route.rs");
