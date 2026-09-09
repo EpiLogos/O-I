d7162f0dcf85491c57652ae2d5fa1a2f4163c4c2
Epii <frank.g.taylor97@gmail.com>
2026-09-10 00:58:21 +0100

    feat: include Development Field suite control plane

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -21,6 +21,7 @@ mod composition {
     include!("rolling_dev.rs");
     include!("existing_world.rs");
     include!("omarchy_host.rs");
+    include!("development_field_command.rs");
     include!("product_command_route.rs");
     include!("desktop_command.rs");
     include!("dev_world_command.rs");
